fbd1af304839b0c06e01794fe5731dd9f9a5d820
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-02-24 11:12:59 +0000

    IO scalaz stuff

--- src/main/scala/io/monad/scalaz/IoMonadApp.scala
+++ src/main/scala/io/monad/scalaz/IoMonadApp.scala
@@ -9,21 +9,28 @@ object IoMonadApp extends App {
   case object Unlucky extends MyException
   case object BadDay  extends MyException
   case object Random  extends MyException
+  case object NumberFormatException extends MyException
 
   def run(args: List[String]) : IO[Nothing, ExitStatus] = {
 
     {for {
-      input      <- putStrLn("Give me a number") *> getStrLn.map(_.toInt)
-      x          <- someComputation(input)
+      input      <- putStrLn("Give me a number") *> getStrLn
+      number     <- mapNumber(input)
+      x          <- someComputation(number)
       otherInput <- putStrLn("Give me another number") *> getStrLn.map(_.toInt)
       y          <- someOtherComputation(otherInput)
       _          <- putStr((x + y).toString)
-    } yield ()}.redeemPure(_ => ExitStatus.ExitNow(1), _ => ExitStatus.ExitNow(2))
+    } yield ()}.run.redeemPure(_ => ExitStatus.ExitNow(1), _ => ExitStatus.ExitNow(2))
   }
 
   def someComputation(input: Int): IO[MyException, Int] = {
     if(input % 2 == 0) IO.now(input * 10)
     else               IO.fail(BadDay)
+  }
+
+  def mapNumber(s: String): IO[MyException, Int] = {
+    if(s.forall(_.isDigit)) IO.now(s.toInt)
+    else IO.fail(NumberFormatException)
   }
 
   def someOtherComputation(max: Int): IO[MyException, Int] = IO.sync(scala.util.Random.nextInt(max))
