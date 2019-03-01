3ef852bb404ed5c6b84f75a9871463baadac6d8b
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-03-01 19:09:03 +0000

    diff implm

--- src/main/scala/applicatives/Applicatives.scala
+++ src/main/scala/applicatives/Applicatives.scala
@@ -3,11 +3,11 @@ package applicatives
 import cats.Applicative
 
 object Applicatives extends App {
-  implicit def optionApplicative = new Applicative[Option] {
+  implicit def optionApplicative: Applicative[Option] = new Applicative[Option] {
     override def pure[A](x: A): Option[A] = Option(x)
 
     override def ap[A, B](ff: Option[A => B])(fa: Option[A]): Option[B] = {
-      fa.flatMap(a => ff.map(f => f(a)))
+      ff.flatMap(f => fa.map(a => f(a)))
     }
   }
 }
