b2878d063a3908a32e3a686643dbdd630ee657da
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-03-02 11:40:06 +0000

    more applicative

--- src/main/scala/applicatives/Applicatives.scala
+++ src/main/scala/applicatives/Applicatives.scala
@@ -1,13 +1,48 @@
 package applicatives
 
 import cats.Applicative
-
+import cats.syntax.applicative._
+import cats.instances.option._
 object Applicatives extends App {
   implicit def optionApplicative: Applicative[Option] = new Applicative[Option] {
     override def pure[A](x: A): Option[A] = Option(x)
 
     override def ap[A, B](ff: Option[A => B])(fa: Option[A]): Option[B] = {
-      ff.flatMap(f => fa.map(a => f(a)))
+      ???
     }
   }
+
+  def func(a: Int, b: String): Int = a
+
+  val x = Option(5)
+  val y = Option("asda")
+
+  val applicativeExample: Option[String => Int] = x.map{ i => s: String => func(i, s)}
+
+  // the way I see it, Applicative is quite close to being a Monad, but not quite so
+  // it doesn't have the ability to deal with some extra computations,
+  // but it does have the ability to combine multiple effects to get a common result
+
+  def traverseOption[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = {
+    as.foldLeft(Option(List.empty[B])){(acc: Option[List[B]], curr: A) =>
+      val res: Option[B] = f(curr)
+
+      Applicative[Option].map2(acc, res)(_ :+ _)
+    }
+  }
+
+  def map2[A, B, Z](fa: Option[A], fb: Option[B])(f: (A, B) => Z)(implicit A: Applicative[Option]): Option[Z] = {
+    A.map(product(fa, fb))(f.tupled)
+  }
+
+  def product[A, B](fa: Option[A], fb: Option[B])(implicit A: Applicative[Option]): Option[(A, B)] = {
+    A.ap(fa.map{a => b: B => (a, b)})(fb)
+  }
+
+  println(product(Some(2), Some(3)))
+
+  def traverse[A, B](list: List[A])(f: A => Option[B]): Option[List[B]] = {
+    ???
+  }
 }
+
